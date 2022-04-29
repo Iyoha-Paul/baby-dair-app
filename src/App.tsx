a4c2b5e56db319fe9aa952d1c72be5f324eaecaa
Iyoha-Paul <Iyohapaul1998@gmail.com>
2022-04-29 12:06:05 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ""}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
