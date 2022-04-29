ba1590142c9f3a1081f32bc30262dbd6c60750f2
Iyoha-Paul <Iyohapaul1998@gmail.com>
2022-04-29 13:56:46 +0100

    finished

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,54 +3,60 @@ import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div className="nav container">
-      <div className="nav--margin"><div className="nav__login container">
-        <form className="nav__login__search">
-          <div className="nav__login__search__inputbar">
-            <i className="fa-solid fa-magnifying-glass nav__login__search__inputbar__icon"></i>
-            <input
-              type="text"
-              required
-              placeholder={"Find Information"}
-              className="nav__login__search__inputbar__input"
-            />
+    <div className="nav">
+      <div className="   ">
+        <div className="nav--margin">
+          <div className="nav__login container">
+            <form className="nav__login__search">
+              <div className="nav__login__search__inputbar">
+                <i className="fa-solid fa-magnifying-glass nav__login__search__inputbar__icon"></i>
+                <input
+                  type="text"
+                  required
+                  placeholder={"Find Information"}
+                  className="nav__login__search__inputbar__input"
+                />
+              </div>
+              <button className="nav__login__search__btn">Search</button>
+            </form>
+            <div className="nav__login__action">
+              <NavLink to="/" className="link">
+                About Baby Dairy
+              </NavLink>
+              <NavLink to="/" className="link">
+                Sign in
+              </NavLink>
+              <NavLink to="/" className="link btn">
+                Sign up
+              </NavLink>
+            </div>
           </div>
-          <button className="nav__login__search__btn">Search</button>
-        </form>
-        <div className="nav__login__action">
-          <NavLink to="/" className="link">
-            About Baby Dairy
-          </NavLink>
-          <NavLink to="/" className="link">
-            Sign in
-          </NavLink>
-          <NavLink to="/" className="link btn">
-            Sign up
-          </NavLink>
         </div>
-      </div></div>
-      <div className="nav__sections container">
-        <NavLink to="/" className="link">
-          Community
-        </NavLink>
-        <NavLink to="/" className="link">
-          Getting Pregnant
-        </NavLink>
-        <NavLink to="/" className="link">
-          Pregnancy
-        </NavLink>
-        <NavLink to="/" className="link">
-          Baby
-        </NavLink>
-        <NavLink to="/" className="link">
-          Health
-        </NavLink>
-        <NavLink to="/" className="link">
-          Video
-        </NavLink>
-        <NavLink to="/" className="link">
-          For you
-        </NavLink>
+        <div className="container ">
+          <div className="nav__sections ">
+            <NavLink to="/" className="link">
+              Community
+            </NavLink>
+            <NavLink to="/" className="link">
+              Getting Pregnant
+            </NavLink>
+            <NavLink to="/" className="link">
+              Pregnancy
+            </NavLink>
+            <NavLink to="/" className="link">
+              Baby
+            </NavLink>
+            <NavLink to="/" className="link">
+              Health
+            </NavLink>
+            <NavLink to="/" className="link">
+              Video
+            </NavLink>
+            <NavLink to="/" className="link">
+              For you
+            </NavLink>
+          </div>
+        </div>
       </div>
     </div>
   );
